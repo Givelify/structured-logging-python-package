accf5d6dc73bcd781c4d54fcd3a18e24727406b6
raymond <rdiep@givelify.com>
2025-07-28 16:58:26 -0600

    remove opinionated format

--- givelifylogging/StructuredLogger.py
+++ givelifylogging/StructuredLogger.py
@@ -9,17 +9,15 @@ from givelifylogging.CustomJsonFormatter import CustomJsonFormatter
 class StructuredLogger:
     def __init__(self, base_logger):
         self.logger = base_logger
-        formatted_log_filename = (
-            f"logFile-{datetime.datetime.now().strftime('%Y-%m-%d')}.log"
-        )
-        self.filepath = os.path.join(".logs", formatted_log_filename)
+        formatted_log_filename = f"logFile-{datetime.datetime.now().strftime('%Y-%m-%d')}.log"
+        self.filepath = os.path.join('.logs', formatted_log_filename)
 
     def setLogFile(self, filepath):
         self.filepath = filepath
-
+    
     def getLogFile(self):
         return self.filepath
-
+    
     def log(
         self, message, type_="generic", value=None, level=logging.INFO, exc_info=False
     ):
@@ -48,15 +46,11 @@ class StructuredLogger:
     def critical(self, message, type_="generic", value=None):
         self.log(message, type_, value, logging.CRITICAL)
 
-    def getLogger(
-        __name__, loglevelstr="INFO", handler=None, folder=".logs", filename="logFile"
-    ):
+    def getLogger(__name__, loglevelstr='INFO', handler=None, folder='.logs', filename='logFile'):
         if not os.path.exists(folder):
             os.makedirs(folder)
 
-        formatted_log_filename = (
-            f"{filename}-{datetime.datetime.now().strftime('%Y-%m-%d')}.log"
-        )
+        formatted_log_filename = f"{filename}-{datetime.datetime.now().strftime('%Y-%m-%d')}.log"
         log_file = os.path.join(folder, formatted_log_filename)
 
         base_logger = logging.getLogger(__name__)
@@ -69,16 +63,11 @@ class StructuredLogger:
         base_logger.setLevel(loggingLevel)
 
         if not handler:
-            handler = TimedRotatingFileHandler(
-                log_file, when="midnight", interval=1, backupCount=3, encoding="utf-8"
-            )
+            handler = TimedRotatingFileHandler(log_file, when="midnight", interval=1, backupCount=3, encoding='utf-8')
             handler.suffix = "%Y-%m-%d.log"
 
         # formatter = CustomJsonFormatter('(message) (levelname) (name) (asctime)', datefmt='%Y-%m-%d %H:%M:%S')
-        formatter = CustomJsonFormatter(
-            '{"message": "%(message)s", "level": "%(levelname)s", "name": "%(name)s", "asctime": "%(asctime)s"}',
-            datefmt="%Y-%m-%d %H:%M:%S",
-        )
+        formatter = CustomJsonFormatter('{"message": "%(message)s", "level": "%(levelname)s", "name": "%(name)s", "asctime": "%(asctime)s"}', datefmt='%Y-%m-%d %H:%M:%S')
 
         handler.setFormatter(formatter)
         base_logger.addHandler(handler)
