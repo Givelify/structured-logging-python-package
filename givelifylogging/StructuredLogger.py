f73e3974305ef8a418c4f6d7c00db53bad3da8a2
raymond <rdiep@givelify.com>
2025-07-28 16:53:38 -0600

    add stacktrace logging

--- givelifylogging/StructuredLogger.py
+++ givelifylogging/StructuredLogger.py
@@ -1,32 +1,37 @@
-import logging
 import datetime
+import logging
 import os
 from logging.handlers import TimedRotatingFileHandler
+
 from givelifylogging.CustomJsonFormatter import CustomJsonFormatter
+
 
 class StructuredLogger:
     def __init__(self, base_logger):
         self.logger = base_logger
-        formatted_log_filename = f"logFile-{datetime.datetime.now().strftime('%Y-%m-%d')}.log"
-        self.filepath = os.path.join('.logs', formatted_log_filename)
+        formatted_log_filename = (
+            f"logFile-{datetime.datetime.now().strftime('%Y-%m-%d')}.log"
+        )
+        self.filepath = os.path.join(".logs", formatted_log_filename)
 
     def setLogFile(self, filepath):
         self.filepath = filepath
-    
+
     def getLogFile(self):
         return self.filepath
-    
-    def log(self, message, type_="generic", value=None, level=logging.INFO):
+
+    def log(
+        self, message, type_="generic", value=None, level=logging.INFO, exc_info=False
+    ):
         if value is None:
             value = {}
         context_data = {
             "givelifyEventId": None,
-            "entity": {
-                "type": type_,
-                "value": value
-            }
+            "entity": {"type": type_, "value": value},
         }
-        self.logger.log(level, message, extra={"context": context_data})
+        self.logger.log(
+            level, message, extra={"context": context_data}, exc_info=exc_info
+        )
 
     def info(self, message, type_="generic", value=None):
         self.log(message, type_, value, logging.INFO)
@@ -34,8 +39,8 @@ class StructuredLogger:
     def warn(self, message, type_="generic", value=None):
         self.log(message, type_, value, logging.WARNING)
 
-    def error(self, message, type_="generic", value=None):
-        self.log(message, type_, value, logging.ERROR)
+    def error(self, message, type_="generic", value=None, exc_info=False):
+        self.log(message, type_, value, logging.ERROR, exc_info=exc_info)
 
     def debug(self, message, type_="generic", value=None):
         self.log(message, type_, value, logging.DEBUG)
@@ -43,11 +48,15 @@ class StructuredLogger:
     def critical(self, message, type_="generic", value=None):
         self.log(message, type_, value, logging.CRITICAL)
 
-    def getLogger(__name__, loglevelstr='INFO', handler=None, folder='.logs', filename='logFile'):
+    def getLogger(
+        __name__, loglevelstr="INFO", handler=None, folder=".logs", filename="logFile"
+    ):
         if not os.path.exists(folder):
             os.makedirs(folder)
 
-        formatted_log_filename = f"{filename}-{datetime.datetime.now().strftime('%Y-%m-%d')}.log"
+        formatted_log_filename = (
+            f"{filename}-{datetime.datetime.now().strftime('%Y-%m-%d')}.log"
+        )
         log_file = os.path.join(folder, formatted_log_filename)
 
         base_logger = logging.getLogger(__name__)
@@ -60,11 +69,16 @@ class StructuredLogger:
         base_logger.setLevel(loggingLevel)
 
         if not handler:
-            handler = TimedRotatingFileHandler(log_file, when="midnight", interval=1, backupCount=3, encoding='utf-8')
+            handler = TimedRotatingFileHandler(
+                log_file, when="midnight", interval=1, backupCount=3, encoding="utf-8"
+            )
             handler.suffix = "%Y-%m-%d.log"
 
         # formatter = CustomJsonFormatter('(message) (levelname) (name) (asctime)', datefmt='%Y-%m-%d %H:%M:%S')
-        formatter = CustomJsonFormatter('{"message": "%(message)s", "level": "%(levelname)s", "name": "%(name)s", "asctime": "%(asctime)s"}', datefmt='%Y-%m-%d %H:%M:%S')
+        formatter = CustomJsonFormatter(
+            '{"message": "%(message)s", "level": "%(levelname)s", "name": "%(name)s", "asctime": "%(asctime)s"}',
+            datefmt="%Y-%m-%d %H:%M:%S",
+        )
 
         handler.setFormatter(formatter)
         base_logger.addHandler(handler)
@@ -73,5 +87,3 @@ class StructuredLogger:
         logger.setLogFile(log_file)
 
         return logger
-    
-
